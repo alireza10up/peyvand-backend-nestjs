21e0c4070850c9ea1ff309a59327b5d0f353ff65
alireza10up <toolsalireza10up@gmail.com>
2025-05-17 21:37:55 +0330

    update users module

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const x = {
+    a: 1,
+    b: 2,
+    c: {
+      abc: {
+        a: 1,
+        b: 2,
+        c: 3,
+      },
+    },
+  };
 }
+
 bootstrap();
